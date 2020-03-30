5d8cf31582a931427bb1442a988b80cb0b8b7b56
Ryan Hartadi <Ryanhartadi06@gmail.com>
2020-03-31 01:52:36 +0700

    tambahan logout dah selesai

--- Project/application/controllers/Auth.php
+++ Project/application/controllers/Auth.php
@@ -78,35 +78,45 @@ defined('BASEPATH') or exit('No direct script access allowed');
             }
         }
         public function registration(){
-        $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[registrasi.email]', [
-            'is_unique' => 'Email Already Registered!'
-        ]);
-        $this->form_validation->set_rules('pass1', 'Password', 'required|trim|min_length[3]|matches[pass2]', [
-            'matches' => 'Password Dont Match!',
-            'min_length' => 'Password too short'
-        ]);
-        $this->form_validation->set_rules('pass2', 'Password', 'required|trim|matches[pass1]');
-        if ($this->form_validation->run() == false) {
-                $data['title'] = "Registration";
-                $this->load->view('templates/auth_header',$data);
-                $this->load->view('auth/register');
-                $this->load->view('templates/auth_footer');
-        }else {
-            $data = [
-                'email' => $this->input->post('email'),
-                'password' => password_hash($this->input->post('pass1'), PASSWORD_DEFAULT),
-                'role_id' => 2,
-                'is_actived' => 0,
-                'nama' => $this->input->post('name'),
-                'create_at' => time()
-            ];
-            $this->db->insert('registrasi', $data);
-            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
-            Your Account has been success created!
-          </div>');
-            redirect('auth/login');
+            $this->form_validation->set_rules('name', 'Name', 'required|trim');
+            $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[registrasi.email]', [
+                'is_unique' => 'Email Already Registered!'
+            ]);
+            $this->form_validation->set_rules('pass1', 'Password', 'required|trim|min_length[3]|matches[pass2]', [
+                'matches' => 'Password Dont Match!',
+                'min_length' => 'Password too short'
+            ]);
+            $this->form_validation->set_rules('pass2', 'Password', 'required|trim|matches[pass1]');
+            if ($this->form_validation->run() == false) {
+                    $data['title'] = "Registration";
+                    $this->load->view('templates/auth_header',$data);
+                    $this->load->view('auth/register');
+                    $this->load->view('templates/auth_footer');
+            }else {
+                $data = [
+                    'email' => $this->input->post('email'),
+                    'password' => password_hash($this->input->post('pass1'), PASSWORD_DEFAULT),
+                    'role_id' => 2,
+                    'is_actived' => 0,
+                    'nama' => $this->input->post('name'),
+                    'create_at' => time()
+                ];
+                $this->db->insert('registrasi', $data);
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+                Your Account has been success created!
+            </div>');
+                redirect('auth/login');
+            }
         }
+        public function logout()
+        {
+            $this->session->unset_userdata('email');
+            $this->session->unset_userdata('role_id');
+    
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Your account has been logged out!
+            </div>');
+            redirect('auth/login');
         }
     }
     ?>
--- Project/application/views/template/sidebar.php
+++ Project/application/views/template/sidebar.php
@@ -9,7 +9,7 @@
     <div class="sidebar-brand-icon rotate-n-15">
       <i class="fas fa-laugh-wink"></i>
     </div>
-    <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+    <div class="sidebar-brand-text mx-3">Admin</div>
   </a>
 
   <!-- Divider -->
@@ -82,8 +82,11 @@
       <i class="fas fa-fw fa-cog"></i>
       <span>Setting Akun</span></a>
   </li>
-
-  
+  <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('auth/logout') ?>" data-toggle="modal" data-target="#logoutModal">
+                <i class="fas fa-sign-out-alt"></i>
+                <span>Logout</span></a>
+        </li>
 
   
 
