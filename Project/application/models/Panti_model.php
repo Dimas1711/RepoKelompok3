78c9ab7fa4e9963a0084fbba6926260f77baddbe
Ratna Dwi <ratnadwikristinasari27@gmail.com>
2020-04-20 20:00:35 +0700

    Model ListPanti
    Update Model

--- Project/application/models/Panti_model.php
+++ Project/application/models/Panti_model.php
@@ -2,13 +2,24 @@
  
 class Panti_model extends CI_Model
 {
-	public function insertdata($data = array()){
-        $this->load->database();
-        return $this->db->insert("panti" , $data);
+	public function index_get(){
+        return $this->db->get('panti')->result_array();
     }
-    public function insertkasus($data = array()){
-        $this->load->database();
-        return $this->db->insert("kasus" , $data);
+    public function insertdata($data = array()){
+        return $this->db->insert('panti' , $data);
     }
+    public function detail($id){
+        return $this->db->get_where("panti" , [
+            'id_panti' => $id
+        ])->result_array();
+    }
+    public function hapusdata($id){
+        $this->db->where('id_panti' , $id);
+        return $this->db->delete('panti');
+    }
+    public function update($data= array(),$id){
+        $this->load->database();
+        return $this->db->update("panti",$data , ["id_panti"=>$id]);
+     }
 }
 ?>
