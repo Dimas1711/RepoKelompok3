310edd18e37d231c52b17ffb8b8accea283270a8
ratnadwikristinasari <ratnadwikristinasari27@gmail.com>
2020-04-22 23:42:36 +0700

    study kasus botstrap

---
+++ E41180434_Ratna/latihan/application/views/template/footer.php
@@ -0,0 +1,47 @@
+  <!-- Footer -->
+  <footer class="footer">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-4">
+          <span class="copyright">Copyright &copy; Bimbel.Co 2020</span>
+        </div>
+        <div class="col-md-4">
+          <ul class="list-inline social-buttons">
+            <li class="list-inline-item">
+              <a href="#">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="<?php echo ("assetss/vendor/jquery/jquery.min.js"); ?>"></script>
+  <script src="<?php echo ("assets/vendor/bootstrap/js/bootstrap.bundle.min.js"); ?>"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="<?php echo ("assets/vendor/jquery-easing/jquery.easing.min.js"); ?>"></script>
+
+  <!-- Contact form JavaScript -->
+  <script src="<?php echo ("assets/js/jqBootstrapValidation.js"); ?>"></script>
+  <script src="<?php echo ("assets/js/contact_me.js"); ?>"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="<?php echo ("asssets/js/agency.min.js"); ?>"></script>
+
+</body>
+
+</html>
---
+++ E41180434_Ratna/latihan/application/views/template/header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Bimbel.Co</title>
+
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/bootstrap/css/bootstrap.min.css">
+
+  <!-- Custom fonts for this template -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/fontawesome-free/css/all.min.css"  type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+  <!-- Custom styles for this template -->
+  <link href="<?php echo base_url(); ?>assets/css/agency.min.css" rel="stylesheet">
+
+</head>
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Bimbel.Co</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav text-uppercase ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#Home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#Pendaftaran">Pendaftaran</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#Siswa">Siswa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#Guru">Guru</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Header -->
+  <header class="masthead">
+    <div class="container">
+      <div class="intro-text">
+        <div class="intro-lead-in">Welcome To Bimbel.Co</div>
+        <div class="intro-heading text-uppercase">Smart and Inovation</div>
+        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Pendaftaran</a>
+      </div>
+    </div>
+  </header>
