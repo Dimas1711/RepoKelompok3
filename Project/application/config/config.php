51592a67f7c6eceeabf28f516b6813f62d210251
Ryan Hartadi <Ryanhartadi06@gmail.com>
2020-04-27 14:16:38 +0700

    Update config.php

--- Project/application/config/config.php
+++ Project/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Semester4/RepoKelompok3/Project/';
+$config['base_url'] = 'http://localhost/RepoKelompok3/Project/';
 
 /*
 |--------------------------------------------------------------------------
