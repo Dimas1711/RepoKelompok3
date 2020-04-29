54840536cada69830483c711bd28d4ebad1d31e6
Dimas1711 <pdimas429@gmail.com>
2020-04-30 02:45:05 +0700

    beranda android

--- Android/Donasiyatim/app/src/main/java/com/example/donasiyatim/HomeFragment.java
+++ Android/Donasiyatim/app/src/main/java/com/example/donasiyatim/HomeFragment.java
@@ -5,12 +5,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.PluralsRes;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -20,20 +26,30 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.donasiyatim.configfile.ServerApi;
+import com.example.donasiyatim.configfile.Util;
 import com.example.donasiyatim.configfile.authdata;
+import com.example.moeidbannerlibrary.banner.BannerLayout;
+import com.example.moeidbannerlibrary.banner.BaseBannerAdapter;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HomeFragment extends Fragment {
     TextView nama_user, saldo;
+    ImageView img;
     Button btn_dompet;
     String id_regis;
     String saldoku;
+    RecyclerView rv;
+    ArrayList<ModelData> dataModelArrayList;
+    ListAdapter listAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
@@ -41,21 +57,92 @@ public class HomeFragment extends Fragment {
         nama_user = v.findViewById(R.id.tv_namauser);
         saldo = v.findViewById(R.id.tv_saldo);
         btn_dompet = v.findViewById(R.id.btn_dompet);
+        rv = v.findViewById(R.id.rv);
+        img = v.findViewById(R.id.img_kasus);
+        BannerLayout banner= v.findViewById(R.id.Banner);
 
-        String jenenge = getActivity().getIntent().getStringExtra("jeneng");
+        List<String> urls = new ArrayList<>();
+        urls.add("https://upload.wikimedia.org/wikipedia/en/2/26/Amazing_World_of_Gumball_Wattersons.png");
+        urls.add("https://img.tek.id/img/content/2020/02/27/26087/caption-instagram-dari-serial-animasi-adventure-time-RibV8P7U9V.jpg");
+        urls.add("https://static.miraheze.org/toxicfandomsandhatedomswiki/thumb/b/b4/Chowder-cartoon.jpg/300px-Chowder-cartoon.jpg");
+        urls.add("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTVZNZ4xt26RhB7guvwLNStEtB3TYCGbZqYzl5B0bTGAE0g6biU&usqp=CAU");
+        BaseBannerAdapter webBannerAdapter = new BaseBannerAdapter(getActivity().getApplicationContext(), urls);
+        webBannerAdapter.setOnBannerItemClickListener(new BannerLayout.OnBannerItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+
+            }
+        });
+            banner.setAdapter(webBannerAdapter);
+
+        //String jenenge = getActivity().getIntent().getStringExtra("jeneng");
 //        id_regis = getActivity().getIntent().getStringExtra("id_registrasi");
 //        Log.e("id_register" ,""+ id_regis);
         String val = authdata.getInstance(getActivity()).getKodeUser();
         Log.e("val","testes" + val);
-        nama_user.setText(jenenge);
+        //nama_user.setText(jenenge);
         loaddetail();
+        retrieveJSON();
 
         return v;
     }
 
+    private void retrieveJSON()
+    {
+        StringRequest stringRequest  = new StringRequest(Request.Method.GET, ServerApi.IPServer + "kasus/index_get", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("strrrrr", ">>" + response);
+                try {
+                    JSONObject obj  = new JSONObject(response);
+
+                    dataModelArrayList = new ArrayList<>();
+                    JSONArray data = obj.getJSONArray("data");
+                    for (int i = 0; i < data.length(); i++)
+                    {
+                        ModelData playerModel = new ModelData();
+                        JSONObject dataobj = data.getJSONObject(i);
+
+                        playerModel.setJudul(dataobj.getString("judul"));
+                        playerModel.setTujuan(Util.setformatrupiah(dataobj.getString("tujuan_dana")));
+
+                        dataModelArrayList.add(playerModel);
+                    }
+                    setupListView();
+
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("volley", "errornya : " + error.getMessage());
+                    }
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        requestQueue.add(stringRequest);
+    }
+
+    private void setupListView()
+    {
+        listAdapter = new ListAdapter(dataModelArrayList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        rv.setLayoutManager(layoutManager);
+        rv.setAdapter(listAdapter);
+    }
+
+
+
     private void loaddetail()//ini buat nampilin saldo
     {
-        StringRequest senddata = new StringRequest(Request.Method.GET, ServerApi.IPServer + "data_user/index_get", new Response.Listener<String>() {
+        StringRequest senddata = new StringRequest(Request.Method.GET, ServerApi.IPServer + "data_user/index_get?id_registrasi="+authdata.getInstance(getActivity()).getKodeUser(), new Response.Listener<String>(){
+                //StringRequest senddata = new StringRequest(Request.Method.GET, ServerApi.IPServer + "data_user/index_get", new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 JSONObject res = null;
@@ -64,7 +151,8 @@ public class HomeFragment extends Fragment {
                     Log.e("responnya ",""+response);
                     JSONArray arr = res.getJSONArray("data");
                     JSONObject arr1 = arr.getJSONObject(0);
-                    saldo.setText(arr1.getString("finansial"));
+                    saldo.setText(Util.setformatrupiah(arr1.getString("finansial")));
+                    nama_user.setText(arr1.getString("nama_user"));
                     saldoku = arr1.getString("finansial");
                     Log.e("saldo", ""+saldoku);
                 } catch (JSONException e) {
@@ -80,12 +168,12 @@ public class HomeFragment extends Fragment {
                     }
                 }) {
 
-            @Override
-            public Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("id_registrasi", authdata.getInstance(getActivity()).getKodeUser());
-                return params;
-            }
+//            @Override
+//            public Map<String, String> getParams() throws AuthFailureError {
+//                Map<String, String> params = new HashMap<String, String>();
+//                params.put("id_registrasi", authdata.getInstance(getActivity()).getKodeUser());
+//                return params;
+//            }
         };
         RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
 
---
+++ Android/Donasiyatim/app/src/main/java/com/example/donasiyatim/ListAdapter.java
@@ -0,0 +1,59 @@
+package com.example.donasiyatim;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.donasiyatim.configfile.ServerApi;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder> {
+
+    private ArrayList<ModelData> dataArrayList;
+
+    public ListAdapter(ArrayList<ModelData> dataArrayList)
+    {
+        this.dataArrayList = dataArrayList;
+    }
+
+
+
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.list_item_kasus, parent,false);
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+        holder.judul.setText(dataArrayList.get(position).getJudul());
+        holder.tujuan_dana.setText(dataArrayList.get(position).getTujuan());
+        //Picasso.get().load(ServerApi.IPServer + dataArrayList.get(position)).into(holder.image);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return (dataArrayList != null) ? dataArrayList.size() : 0;
+    }
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+        private TextView judul,tujuan_dana;
+        private ImageView image;
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            judul = itemView.findViewById(R.id.tv_judul);
+            tujuan_dana = itemView.findViewById(R.id.tv_tujuan_dana);
+            image = itemView.findViewById(R.id.img_kasus);
+        }
+    }
+}
