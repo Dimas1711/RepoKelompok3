f1131b0a53d1d6348ea74be59b309481c5c937c1
Dimas1711 <pdimas429@gmail.com>
2020-05-02 01:20:52 +0700

    update android

--- Android/Donasiyatim/app/src/main/java/com/example/donasiyatim/ListAdapter.java
+++ Android/Donasiyatim/app/src/main/java/com/example/donasiyatim/ListAdapter.java
@@ -1,5 +1,6 @@
 package com.example.donasiyatim;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,8 +38,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder
     public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
         holder.judul.setText(dataArrayList.get(position).getJudul());
         holder.tujuan_dana.setText(dataArrayList.get(position).getTujuan());
-        //Picasso.get().load(ServerApi.IPServer + dataArrayList.get(position)).into(holder.image);
-
+        //Picasso.get().load(ServerApi.IPServer + "../" + dataArrayList.get(position).getImage()).into(holder.image);
     }
 
     @Override
@@ -49,6 +49,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder
     public class ListViewHolder extends RecyclerView.ViewHolder {
         private TextView judul,tujuan_dana;
         private ImageView image;
+
         public ListViewHolder(@NonNull View itemView) {
             super(itemView);
             judul = itemView.findViewById(R.id.tv_judul);
