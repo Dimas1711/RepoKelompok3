c13f1786e2bb3837fa8e183dcee37b85f97250e3
ratnadwikristinasari <ratnadwikristinasari27@gmail.com>
2020-05-02 22:23:18 +0700

    Revert "Merge branch 'master' of https://github.com/Dimas1711/RepoKelompok3"
    This reverts commit b61bcebe4b3cc32c99fb001023d91786ad19275d, reversing
    changes made to 129cb736914727ffeb04660af60d49b5a60ff4db.

--- Android/Donasiyatim/app/src/main/java/com/example/donasiyatim/ListAdapter.java
+++ Android/Donasiyatim/app/src/main/java/com/example/donasiyatim/ListAdapter.java
@@ -1,6 +1,5 @@
 package com.example.donasiyatim;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,7 +37,8 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder
     public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
         holder.judul.setText(dataArrayList.get(position).getJudul());
         holder.tujuan_dana.setText(dataArrayList.get(position).getTujuan());
-        //Picasso.get().load(ServerApi.IPServer + "../" + dataArrayList.get(position).getImage()).into(holder.image);
+        //Picasso.get().load(ServerApi.IPServer + dataArrayList.get(position)).into(holder.image);
+
     }
 
     @Override
@@ -49,7 +49,6 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder
     public class ListViewHolder extends RecyclerView.ViewHolder {
         private TextView judul,tujuan_dana;
         private ImageView image;
-
         public ListViewHolder(@NonNull View itemView) {
             super(itemView);
             judul = itemView.findViewById(R.id.tv_judul);
