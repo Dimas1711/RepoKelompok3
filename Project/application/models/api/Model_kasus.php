5b6fd3d81604a0bf50bb689381a84ea5bed2b1f3
Ryan Hartadi <Ryanhartadi06@gmail.com>
2020-05-06 21:35:22 +0700

    Update Model_kasus.php

--- Project/application/models/api/Model_kasus.php
+++ Project/application/models/api/Model_kasus.php
@@ -7,9 +7,9 @@
         // }
         public function index($id = null){
             if ($id === null) {
-                 return $this->db->get('kasus')->result_array();
+                return $this->db->get_where('kasus' , ['status' => 1])->result_array();
             }else {
-                return $this->db->get_where('kasus' , ['id_kasus' => $id])->result_array();
+                return $this->db->get_where('kasus' , ['id_kasus' => $id , 'status' => 1])->result_array();
             }
         }
         
