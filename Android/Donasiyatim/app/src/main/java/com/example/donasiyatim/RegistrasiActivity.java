6f43e392b8b633ed6389f498017ab65062c702a5
ratnadwikristinasari <ratnadwikristinasari27@gmail.com>
2020-06-22 11:52:49 +0700

    update regis

--- Android/Donasiyatim/app/src/main/java/com/example/donasiyatim/RegistrasiActivity.java
+++ Android/Donasiyatim/app/src/main/java/com/example/donasiyatim/RegistrasiActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,18 +23,22 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.donasiyatim.configfile.ServerApi;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class RegistrasiActivity extends AppCompatActivity {
 
-    EditText fullname, pass, konfirm, email;
+    EditText fullname, pass, konfirm, email, nomer;
     Button register;
     RequestQueue requestQueue;
     String NameHolder, EmailHolder, PasswordHolder ;
-    ProgressDialog progressDialog;
     Boolean CheckEditText;
     TextView masuk;
+    ProgressDialog progressDialog;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +48,11 @@ public class RegistrasiActivity extends AppCompatActivity {
         email = findViewById(R.id.edt_email);
         pass = findViewById(R.id.edt_password);
         konfirm = findViewById(R.id.edt_konfrimasi);
+        nomer = findViewById(R.id.edt_nomor);
         masuk = findViewById(R.id.masuk);
         requestQueue = Volley.newRequestQueue(RegistrasiActivity.this);
-        progressDialog = new ProgressDialog(RegistrasiActivity.this);
+        progressDialog = new ProgressDialog(this);
+        progressBar = new ProgressBar(RegistrasiActivity.this);
         masuk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,70 +64,97 @@ public class RegistrasiActivity extends AppCompatActivity {
         register = findViewById(R.id.btn_daftar);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                checkedittext();
-                progressDialog.setMessage("Tunggu Sebentar");
-                progressDialog.show();
-                if (!pass.getText().toString().equals(konfirm.getText().toString())){
-                    Toast.makeText(RegistrasiActivity.this , "Password Yang Dimasukkan Salah", Toast.LENGTH_SHORT).show();
-                    progressDialog.dismiss();
-                }else if (CheckEditText){
-                    setRegister();
-                    Intent login = new Intent(RegistrasiActivity.this , LoginActivity.class);
-                    startActivity(login);
-                    finish();
-                }else {
-
-                    Toast.makeText(RegistrasiActivity.this , "Isi Seluruh Data" , Toast.LENGTH_SHORT).show();
-                    progressDialog.dismiss();
-                }
-
+            public void onClick(View v){
+                UserRegistration();
             }
         });
-
     }
-    public void setRegister(){
-        progressDialog.setMessage("Tunggu Sebentar , Data Anda Sedang Ditambahkan Ke Dalam Server");
-        progressDialog.show();
-        StringRequest stringRequest =new StringRequest(Request.Method.POST, ServerApi.IPServer+"registrasi/index_get",
+
+    public void UserRegistration() {
+        final String nama_user = this.fullname.getText().toString().trim();
+        final String email = this.email.getText().toString().trim();
+        final String no_telp = this.nomer.getText().toString().trim();
+        final String password = this.pass.getText().toString().trim();
+        final String konfirm_pass = this.konfirm.getText().toString().trim();
+
+        if (nama_user.matches("")){
+            Toast.makeText(this, "Masukkan Nama Lengkap Anda", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (email.matches("")){
+            Toast.makeText(this, "Masukkan email Anda", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (no_telp.matches("")){
+            Toast.makeText(this, "Masukkan No Telpon Anda", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (password.matches("")){
+            Toast.makeText(this, "Masukkan password Anda", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (konfirm_pass.matches("")) {
+            Toast.makeText(this, "Masukkan password Anda lagi", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressBar.setVisibility(View.GONE);
+        masuk.setVisibility(View.GONE);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, ServerApi.URL_REGIS,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        progressDialog.dismiss();
-                        Toast.makeText(RegistrasiActivity.this, "Registrasi Berhasil", Toast.LENGTH_SHORT).show();
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String status = jsonObject.getString("status");
+                            String error = jsonObject.getString("error");
+                            String message = jsonObject.getString("message");
+
+                            if (status.equals("200") && error.equals("false")) {
+                                Toast.makeText(RegistrasiActivity.this, message, Toast.LENGTH_SHORT).show();
+                                new Handler().postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Intent intent2 = new Intent(RegistrasiActivity.this, LoginActivity.class);
+                                        startActivity(intent2);
+                                    }
+                                }, 1500);
+                            } else {
+                                Toast.makeText(RegistrasiActivity.this, message, Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
+                                masuk.setVisibility(View.VISIBLE);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            progressBar.setVisibility(View.GONE);
+                            Intent intent3 = new Intent(RegistrasiActivity.this, LoginActivity.class);
+                            startActivity(intent3);
+                            Toast.makeText(RegistrasiActivity.this, "Registrasi Berhasil", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        progressDialog.dismiss();
-                        Log.e("volley" , error.toString());
-                        Toast.makeText(RegistrasiActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(RegistrasiActivity.this, "Error! " + error.toString(), Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+                        masuk.setVisibility(View.VISIBLE);
                     }
-                }){
-            protected Map<String, String> getParams(){
-                Map<String , String> params = new HashMap<>();
-                params.put("nama" ,NameHolder);
-                params.put("email" , EmailHolder);
-                params.put("password" , PasswordHolder);
-
+                })
+        {
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("nama_user",nama_user);
+                params.put("email",email);
+                params.put("no_telp",no_telp);
+                params.put("password",password);
                 return params;
             }
         };
-        RequestQueue requestQueue = Volley.newRequestQueue(RegistrasiActivity.this);
 
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
     }
-    public void checkedittext(){
-        NameHolder = fullname.getText().toString().trim();
-        EmailHolder = email.getText().toString().trim();
-        PasswordHolder = pass.getText().toString().trim();
 
-        if (TextUtils.isEmpty(NameHolder) || TextUtils.isEmpty(EmailHolder) || TextUtils.isEmpty(PasswordHolder)){
-            CheckEditText = false;
-        } else {
-            CheckEditText = true;
-        }
-    }
 
 }
--- Android/Donasiyatim/app/src/main/java/com/example/donasiyatim/configfile/ServerApi.java
+++ Android/Donasiyatim/app/src/main/java/com/example/donasiyatim/configfile/ServerApi.java
@@ -1,14 +1,16 @@
 package com.example.donasiyatim.configfile;
 
 public class ServerApi {
-   //public static final String IPServer="http://192.168.1.7/RepoKelompok3/Project/api/";
+   //public static final String IPServer="http://192.168.1.11/RepoKelompok3/Project/api/";
    //public static final String IPServer="http://192.168.43.173/RepoKelompok3/Project/api/";
-    public static final String IPServer="http://192.168.1.4/RepoKelompok3/Project/api/";
+   // public static final String IPServer="http://192.168.1.4/RepoKelompok3/Project/api/";
+    public static final String IPServer="http://192.168.43.76/RepoKelompok3/Project/api/";
     public static final String URL_LOGIN=IPServer+"auth/index_post";
     public static final String URL_LOGOUT=IPServer+"Api/authlogout";
     public static final String URL_SAVE_DATA=IPServer+"Api/savedata";
     public static final String URL_GET_DATA=IPServer+"Api/getdataresult";
     public static final String URL_GET_DETAIL_DATA=IPServer+"Api/getdetaildata";
     public static final String URL_LIST_ITEM = IPServer+"kasus/index_get";
+    public static final String URL_REGIS = IPServer+"registrasi";
 
 }
