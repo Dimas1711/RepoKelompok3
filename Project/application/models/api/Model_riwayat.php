6cc226196b6ceb4f63a8bdbcfd3abe9fad547057
Ryan Hartadi <Ryanhartadi06@gmail.com>
2020-06-23 07:07:59 +0700

    Update Model_riwayat.php

--- Project/application/models/api/Model_riwayat.php
+++ Project/application/models/api/Model_riwayat.php
@@ -8,7 +8,8 @@
             // }else {
             //     return $this->db->get_where('donasi' , ['id_user' => $id])->result_array();
             // }
-            return $this->db->get_where('donasi' , ['id_user' => $id])->result_array();
-
+            // return $this->db->get_where('donasi' , ['id_user' => $id])->result_array();
+            $q = $this->db->query("SELECT * FROM donasi , kasus WHERE donasi.id_kasus = kasus.id_kasus AND id_user = '$id'")->result_array();
+            return $q;
         }
     }
